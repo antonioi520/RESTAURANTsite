1e4b4f890610b51ae7ffc489a058661a4ded8cf0
antonio <prodoson@gmail.com>
2019-06-18 16:35:42 -0400

    menu items and css + gallery css

--- gallery.php
+++ gallery.php
@@ -29,39 +29,41 @@
 
 
 <?php include("views/nav_pages.html")?>
-<br>
 
-<h1 style="text-align:Center">Gallery</h1>
-<br>
-<hr>
+
+
 
 <div class="wrapper">
+    <br>
+    <h1 class="menu-heading">Gallery</h1>
+    <br>
+    <hr>
     <h2 style="text-align:left">Food & Drink</h2>
     <hr>
     <div class="row">
         <div class="column">
-            <img src="img/FoodDrink/1.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(1)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/FoodDrink/1.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(1)" class="hover-shadow cursor">
         </div>
         <div class="column">
-            <img src="img/FoodDrink/2.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(2)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/FoodDrink/2.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(2)" class="hover-shadow cursor">
         </div>
         <div class="column">
-            <img src="img/FoodDrink/3.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(3)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/FoodDrink/3.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(3)" class="hover-shadow cursor">
         </div>
         <div class="column">
-            <img src="img/FoodDrink/4.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(4)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/FoodDrink/4.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(4)" class="hover-shadow cursor">
         </div>
         <div class="column">
-            <img src="img/FoodDrink/5.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(5)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/FoodDrink/5.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(5)" class="hover-shadow cursor">
         </div>
         <div class="column">
-            <img src="img/FoodDrink/6.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(6)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/FoodDrink/6.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(6)" class="hover-shadow cursor">
         </div>
         <div class="column">
-            <img src="img/FoodDrink/7.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(7)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/FoodDrink/7.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(7)" class="hover-shadow cursor">
         </div>
         <div class="column">
-            <img src="img/FoodDrink/8.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(8)" class="hover-shadow cursor">
+            <img sid="galleryImg" rc="img/FoodDrink/8.jpg" style="width:100%" onclick="openFoodModal();currentFoodSlide(8)" class="hover-shadow cursor">
         </div>
     </div>
 
@@ -71,10 +73,10 @@
     <hr>
     <div class="row">
         <div class="column">
-            <img src="img/Interior/interior.jpg" style="width:100%" onclick="openInteriorModal();currentInteriorSlide(1)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/Interior/interior.jpg" style="width:100%" onclick="openInteriorModal();currentInteriorSlide(1)" class="hover-shadow cursor">
         </div>
         <div class="column">
-            <img src="img/Interior/interior2.jpg" style="width:100%" onclick="openInteriorModal();currentInteriorSlide(2)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/Interior/interior2.jpg" style="width:100%" onclick="openInteriorModal();currentInteriorSlide(2)" class="hover-shadow cursor">
         </div>
     </div>
 
@@ -84,10 +86,10 @@
     <hr>
     <div class="row">
         <div class="column">
-            <img src="img/Staff/staff.jpg" style="width:100%" onclick="openStaffModal();currentStaffSlide(1)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/Staff/staff.jpg" style="width:100%" onclick="openStaffModal();currentStaffSlide(1)" class="hover-shadow cursor">
         </div>
         <div class="column">
-            <img src="img/Staff/staff3.jpg" style="width:100%" onclick="openStaffModal();currentStaffSlide(2)" class="hover-shadow cursor">
+            <img id="galleryImg" src="img/Staff/staff3.jpg" style="width:100%" onclick="openStaffModal();currentStaffSlide(2)" class="hover-shadow cursor">
         </div>
     </div>
 
@@ -97,42 +99,42 @@
 
             <div class="foodSlides">
                 <div class="numbertext">1 / 8</div>
-                <img src="img/FoodDrink/1.jpg" style="width:100%">
+                <img id="galleryImg" src="img/FoodDrink/1.jpg" style="width:100%">
             </div>
 
             <div class="foodSlides">
                 <div class="numbertext">2 / 8</div>
-                <img src="img/FoodDrink/2.jpg" style="width:100%">
+                <img id="galleryImg" src="img/FoodDrink/2.jpg" style="width:100%">
             </div>
 
             <div class="foodSlides">
                 <div class="numbertext">3 / 8</div>
-                <img src="img/FoodDrink/3.jpg" style="width:100%">
+                <img id="galleryImg" src="img/FoodDrink/3.jpg" style="width:100%">
             </div>
 
             <div class="foodSlides">
                 <div class="numbertext">4 / 8</div>
-                <img src="img/FoodDrink/4.jpg" style="width:100%">
+                <img id="galleryImg" src="img/FoodDrink/4.jpg" style="width:100%">
             </div>
 
             <div class="foodSlides">
                 <div class="numbertext">5 / 8</div>
-                <img src="img/FoodDrink/5.jpg" style="width:100%">
+                <img id="galleryImg" src="img/FoodDrink/5.jpg" style="width:100%">
             </div>
 
             <div class="foodSlides">
                 <div class="numbertext">6 / 8</div>
-                <img src="img/FoodDrink/6.jpg" style="width:100%">
+                <img id="galleryImg" src="img/FoodDrink/6.jpg" style="width:100%">
             </div>
 
             <div class="foodSlides">
                 <div class="numbertext">7 / 8</div>
-                <img src="img/FoodDrink/7.jpg" style="width:100%">
+                <img id="galleryImg" src="img/FoodDrink/7.jpg" style="width:100%">
             </div>
 
             <div class="foodSlides">
                 <div class="numbertext">8 / 8</div>
-                <img src="img/FoodDrink/8.jpg" style="width:100%">
+                <img id="galleryImg" src="img/FoodDrink/8.jpg" style="width:100%">
             </div>
 
             <a class="prev" onclick="plusFoodSlides(-1)">&#10094;</a>
@@ -147,12 +149,12 @@
 
             <div class="interiorSlides">
                 <div class="numbertext">1 / 2</div>
-                <img src="img/Interior/interior.jpg" style="width:100%">
+                <img id="galleryImg" src="img/Interior/interior.jpg" style="width:100%">
             </div>
 
             <div class="interiorSlides">
                 <div class="numbertext">2 / 2</div>
-                <img src="img/Interior/interior2.jpg" style="width:100%">
+                <img id="galleryImg" src="img/Interior/interior2.jpg" style="width:100%">
             </div>
 
             <a class="prev" onclick="plusInteriorSlides(-1)">&#10094;</a>
@@ -167,12 +169,12 @@
 
             <div class="staffSlides">
                 <div class="numbertext">1 / 2</div>
-                <img src="img/Staff/staff.jpg" style="width:100%">
+                <img id="galleryImg" src="img/Staff/staff.jpg" style="width:100%">
             </div>
 
             <div class="staffSlides">
                 <div class="numbertext">2 / 2</div>
-                <img src="img/Staff/staff3.jpg" style="width:100%">
+                <img id="galleryImg" src="img/Staff/staff3.jpg" style="width:100%">
             </div>
 
             <a class="prev" onclick="plusStaffSlides(-1)">&#10094;</a>

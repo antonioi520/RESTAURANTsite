f607775fa6ba3460e51aa09919d0234533058857
Luciano728 <lpm72896@gmail.com>
2019-06-19 16:30:20 -0400

    added antwans nice ass carousel to gallery

--- gallery.php
+++ gallery.php
@@ -18,6 +18,7 @@
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/css.css" rel="stylesheet">
     <link href="css/gallery.css" rel="stylesheet">
+    <link href="css/menu.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/v4-shims.css">
@@ -30,6 +31,38 @@
 
 
 <?php include("views/nav_pages.html")?>
+
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel" style="top:-25px;">
+    <div class="carousel-inner">
+        <!--        <div class="carousel-item active">-->
+        <!--            <img class="d-block w-100" src="img/FoodDrink/5.jpg" alt="First slide" style="height:600px;filter:brightness(70%);object-fit: cover">-->
+        <!--        </div>-->
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="img/Interior/interior.jpg" alt="Second slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption d-none d-md-block">
+                <center>
+                    <img src="img/logo.png" class="galleryHeaderImage">
+                </center>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="img/Interior/interior2.jpg" alt="Third slide" style="height:600px;filter:brightness(70%);object-fit: cover">
+            <div class="carousel-caption d-none d-md-block">
+                <center>
+                    <img src="img/logo.png" class="galleryHeaderImage">
+                </center>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
 
 
 

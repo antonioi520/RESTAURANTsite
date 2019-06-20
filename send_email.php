d940bd5bc4bed141593d50a159906080d39f79bc
antonio <prodoson@gmail.com>
2019-06-20 17:35:14 -0400

    sendemail autoload for phpmailer tht isnt used

--- send_email.php
+++ send_email.php
@@ -2,25 +2,27 @@
 <!-- the captcha needs the above line -->
 
 <?php
-if(isset($_POST['email']))
+if(isset($_POST['email'])) {
 
     $email_to = "younglubackup@gmail.com";
     $email_subject = "Mi Alma Cafe Email Test";
 
-    function died($error) {
+    function died($error)
+    {
         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
         echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
+        echo $error . "<br /><br />";
         echo "Please fix before resubmitting.<br /><br />";
         die();
     }
 
     // validation
-    if(!isset($_POST['name']) ||
+    if (!isset($_POST['name']) ||
         !isset($_POST['email']) ||
         !isset($_POST['subject']) ||
-        !isset($_POST['message'])) {
-            died('The message you entered does not appear to be valid.');
+        !isset($_POST['message'])
+    ) {
+        died('The message you entered does not appear to be valid.');
     }
 
     $name = $_POST['name'];
@@ -31,21 +33,21 @@ if(isset($_POST['email']))
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
-    if(!preg_match($email_exp,$email_from)) {
-      $error_message .= 'Please enter a valid email.<br />';
+    if (!preg_match($email_exp, $email_from)) {
+        $error_message .= 'Please enter a valid email.<br />';
     }
 
     $string_exp = "/^[A-Za-z .'-]+$/";
 
-    if(!preg_match($string_exp,$name)) {
-      $error_message .= 'Please enter at least 4 characters.<br />';
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'Please enter at least 4 characters.<br />';
     }
 
-    if(strlen($message) < 2) {
+    if (strlen($message) < 2) {
         $error_message .= 'The message you entered does not appear to be valid.<br />';
     }
 
-    if(strlen($error_message) > 0) {
+    if (strlen($error_message) > 0) {
         died($error_message);
     }
 
@@ -83,26 +85,27 @@ if(isset($_POST['email']))
 
     $email_message = "Form details below.\n\n";
 
-    function clean_string($string) {
-        $bad = array("content-type","bcc:","to:","cc:","href");
-        return str_replace($bad,"",$string);
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
     }
 
-    $email_message .= "Name: ".clean_string($name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Subject: ".clean_string($subject)."\n";
-    $email_message .= "Message: ".clean_string($message)."\n";
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email_from) . "\n";
+    $email_message .= "Subject: " . clean_string($subject) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
 
     // create email headers
-    $headers = 'From: '.$email_from."\r\n".
-        'Reply-To: '.$email_from."\r\n" .
+    $headers = 'From: ' . $email_from . "\r\n" .
+        'Reply-To: ' . $email_from . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
     @mail($email_to, $email_subject, $email_message, $headers);
-
+}
     ?>
      
     <!-- include success html here -->
      
     Your message has been sent, thank you!
      
-}
+

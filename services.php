e63cd9729a7520496c7b67cfda473d4c24297794
Luciano728 <lpm72896@gmail.com>
2019-06-21 13:23:24 -0400

    workin on services page style and info

--- services.php
+++ services.php
@@ -17,11 +17,11 @@
     <!-- Bootstrap -->
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/css.css" rel="stylesheet">
-    <link href="css/gallery.css" rel="stylesheet">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/v4-shims.css">
-
+    <link href="lib/animate-css/animate.min.css" rel="stylesheet">
 
 
 </head>
@@ -31,10 +31,114 @@
 
 <?php include("views/nav_pages.html")?>
 
-<div class="wrapper">
-    <center><img src="img/wip.png"></center>
-</div>
 
+
+<section id="about">
+    <div class="container wow fadeInUp">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="section-title" style="font-family: 'Satisfy', cursive;letter-spacing: 0.05em;font-size: 3.125em;">Services</h3>
+                <div class="section-title-divider"></div>
+                <!--                <p class="section-description">We are a two man team from different backgrounds in programming coming together to deliver you top-notch websites and other business needs!</p>-->
+            </div>
+        </div>
+    </div>
+    <div class="container about-container wow fadeInUp">
+        <div class="row">
+            <div class="col-lg-6 about-img">
+                <img src="img/Staff/MiAlmaCafeOpeningCeremony-8.jpg" alt="" style="height:638px;object-fit: cover;" id="aboutImg">
+            </div>
+
+            <div class="col-md-6 about-content">
+                <h2 class="about-title">Chef's Recommendations</h2>
+                <p class="about-text">
+                    Chef recommendations, weekly specials, a la carte, take out
+                </p>
+
+                <br>
+
+                <h2 class="about-title">Weekly Specials</h2>
+                <p class="about-text">
+                    Our catering services are not limited to just events hosted at the Bomes Theater, we also cater for various other events including weddings and parties.
+                </p>
+
+                <br>
+
+                <h2 class="about-title">Take Out</h2>
+                <p class="about-text">
+                    Give us a call at (401) 648-0715 and we'll prepare your order for you! Pick it up on location at 1017 Broad Street Providence, RI 02905.
+                </p>
+
+                <a href="menu.php" id="ingredientBox"><h2 id="ingredientBoxText">View Menu</h2></a>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="middleImages">
+    <div class="wrapper">
+        <div class="row" style="overflow: hidden;">
+            <div class="col-lg-6">
+                <div style="margin: auto;width: 50%;position: absolute;z-index: 1;left:25%;top:30%;">
+                    <img src="img/imgheader3.png" style="object-fit: contain;">
+                    <h1 style="color:white;font-weight: bolder;padding-top: 2%;margin-bottom: 0;">CATERING AT THE BOMES THEATER</h1>
+                    <strong style="color:white;letter-spacing: 0.05em;"></strong><br><br><br>
+                    <!--a href="menu.php" id="ingredientBox"><h2 id="ingredientBoxText">View Menu</h2></a-->
+                </div>
+                <img class="testy" src="img/TheaterInterior/concert-41.jpg" alt="food" style="height:43.750em;width: 62.500em;object-fit: cover;filter:brightness(30%);float:left;">
+            </div>
+            <div class="col-lg-6">
+                <div style="margin: auto;width: 50%;position: absolute;z-index: 1;left:25%;top:30%;">
+                    <img src="img/imgheader3.png">
+                    <h1 style="color:white;font-weight: bolder;padding-top: 2%;margin-bottom: 0;">CATERING FOR PERSONAL EVENTS</h1>
+                    <strong style="color:white;letter-spacing: 0.05em;"></strong><br><br><br>
+                    <!--a href="services.php" id="ingredientBox" style="width:15.000em;"><h2 id="ingredientBoxText">View Services</h2></a-->
+                </div>
+                <img class="testy" src="img/FoodDrink/MiAlmaCafe-23.jpg" alt="" style="height:43.750em;width: 62.500em;object-fit: cover;filter:brightness(30%);">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="about">
+    <div class="container wow fadeInUp">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="section-title" style="font-family: 'Satisfy', cursive;letter-spacing: 0.05em;font-size: 3.125em;">Catering</h3>
+                <div class="section-title-divider"></div>
+                <!--                <p class="section-description">We are a two man team from different backgrounds in programming coming together to deliver you top-notch websites and other business needs!</p>-->
+            </div>
+        </div>
+    </div>
+    <div class="container about-container wow fadeInUp">
+        <div class="row">
+            <div class="col-lg-6 about-img">
+                <img src="img/FoodDrink/MiAlmaCafe-7.jpg" alt="" style="height:638px;object-fit: cover;" id="aboutImg">
+            </div>
+
+            <div class="col-md-6 about-content">
+                <h2 class="about-title">Catering at the Bomes Theater.</h2>
+                <p class="about-text">
+                    At Mi Alma Restaurant, we provide an array of catering services for our customers.
+                    If you are looking to host an event at the Bomes Theater, we can also
+                    cater for your event!
+                </p>
+
+                <br>
+
+                <h2 class="about-title">Catering for personal events.</h2>
+                <p class="about-text">
+                    Our catering services are not limited to just events hosted at the Bomes Theater, we also cater for various other events including weddings
+                    and parties.
+                </p>
+
+                <br>
+
+            </div>
+        </div>
+    </div>
+</section>
 
 <br>
 <?php include("views/footer.html")?>

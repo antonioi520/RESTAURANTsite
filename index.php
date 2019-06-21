20b2cb9df1ac7187c48f2a2af1842daf1b5f39d0
antonio <prodoson@gmail.com>
2019-06-21 18:04:28 -0400

    home and services changes

--- index.php
+++ index.php
@@ -22,12 +22,12 @@
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="img/Interior/interior2.jpg" class="d-block w-100" alt="..." style="height:59.375em;filter:brightness(20%);object-fit: cover;">
-                <div class="carousel-caption d-md-block" id="hero" style="top:12.500em;">
+                <div class="carousel-caption d-md-block" id="hero" style="top:150px;">
                     <center>
-                        <h1 class="headertext">WELCOME TO MI ALMA<br> RESTAURANT</H1>
+                        <h1 class="headertext" style="margin-bottom: 190px;">WELCOME TO MI ALMA<br> RESTAURANT</H1>
                     </center>
                     <center>
-                        <img src="img/logo.png" style="height:30%;width: 30%;z-index: 1;object-fit: contain;filter:brightness(95%);margin-top:-6em;margin-bottom:4em;">
+                        <img src="img/logo.png" style="height:30%;width: 30%;z-index: 1;object-fit: contain;filter:brightness(95%);margin-top:-6em;margin-bottom:10px;">
                     </center>
                     <center>
                         <div href="#contact" id="box" style="margin-top: 1em">

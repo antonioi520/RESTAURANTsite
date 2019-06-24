93e70fa17944690e0d964fd6d0f8b3e678f6abbd
Luciano728 <lpm72896@gmail.com>
2019-06-24 14:08:22 -0400

    made the email point to MiAlmaRestaurant1 gmail, changed subject text from cafe to restaurant

--- send_email.php
+++ send_email.php
@@ -4,8 +4,8 @@
 <?php
 if(isset($_POST['email'])) {
 
-    $email_to = "younglubackup@gmail.com";
-    $email_subject = "Mi Alma Cafe - " . $_POST['subject'];
+    $email_to = "MiAlmaRestaurant1@gmail.com";
+    $email_subject = "Mi Alma Restaurant - " . $_POST['subject'];
 
     function died($error)
     {

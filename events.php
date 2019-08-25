bfbbccea78315815a11acbe9622fb5a56f895ab5
Luciano728 <lpm72896@gmail.com>
2019-08-25 14:59:24 -0400

    add two new events to events page, delete old events and vids from page as per client request

--- events.php
+++ events.php
@@ -182,27 +182,49 @@
         <div class="section-title-divider" style="width:300px;"></div>
     </div>
     <div class="row" id="videoRow" style="display: inline; padding-top: -10px;">
-        <center>
+        <!--center>
             <video id="video" style="margin-bottom: 30px" controls>
                 <source src="vid/event_video2.mp4" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
-            <!--img src="img/logo.png" style="padding-bottom: 200px;" class="eventHeaderImage""-->
-        </center>
+            <!--img src="img/logo.png" style="padding-bottom: 200px;" class="eventHeaderImage"">
+        </center-->
     </div>
 
     <div class="row" style="display:inline;">
         <div class="card-deck" style="width:80%;margin:auto;">
-            <div class="col-md-3"></div>
             <div class="card" style="border-color: deepskyblue">
-                <img class="card-img-top" src="img/Events/event3.jpeg" alt="Card image cap" style="height:400px;object-fit: contain;">
+                <img class="card-img-top" src="img/Events/event6.jpg" alt="Card image cap" style="height:400px;object-fit: contain;">
                 <div class="card-body" style="padding-left:0;padding-right:0;padding-bottom:0;">
-                    <h5 class="card-title" style="font-weight: bold;padding-left:10px;padding-right:10px;">El Varon De La Bachata</h5>
-                    <p class="card-text" style="background-color: #D0FBFF; color=black;padding-left:10px;padding-right:10px;">Music by DJ Travieso
+                    <h5 class="card-title" style="font-weight: bold;padding-left:10px;padding-right:10px;">Miss Dominican Republic 2019</h5>
+                    <p class="card-text" style="background-color: #D0FBFF; color=black;padding-left:10px;padding-right:10px;">
+                        La Destacada Chef Dominicana Keiry Luciano.
                         <br><br>
-                        <strong>Date: June 28th</strong><br>
-                        <strong>Tickets: $30</strong><br>
-                        <strong>Start: 9pm</strong><br>
+                        <strong>Date: Sunday, September 1st</strong><br>
+                        <strong>Start: 12pm</strong><br>
+                        <strong>End: 10pm</strong><br>
+                    </p>
+
+                </div>
+                <div id="content-desktop" class="card-footer" style="background-color: deepskyblue;height:50px;">
+                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
+                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
+                </div>
+
+                <div id="content-mobile" class="card-footer" style="background-color: deepskyblue;height:60px;">
+                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
+                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
+                </div>
+            </div>
+            <div class="card" style="border-color: deepskyblue">
+                <img class="card-img-top" src="img/Events/event7.jpg" alt="Card image cap" style="height:400px;object-fit: contain;">
+                <div class="card-body" style="padding-left:0;padding-right:0;padding-bottom:0;">
+                    <h5 class="card-title" style="font-weight: bold;padding-left:10px;padding-right:10px;">Miss Dominican Republic 2019</h5>
+                    <p class="card-text" style="background-color: #D0FBFF; color=black;padding-left:10px;padding-right:10px;">
+                        Trophies, prizes, awards and delivery party.
+                        <br><br>
+                        <strong>Date: Saturday, September 21th</strong><br>
+                        <strong>Start: 6pm</strong><br>
                         <strong>End: 1am</strong><br>
                     </p>
 
@@ -217,84 +239,12 @@
                         The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
                 </div>
             </div>
-            <div class="col-md-3"></div>
-
         </div>
     </div>
     <div class="col-md-6"></div>
 
 
 </div>
-    <div class="col-md-6"></div>
-</div>
-
-
-<div class="container">
-
-    <hr style="margin-bottom:0px;">
-    <div class="col-md-6"></div>
-    <div class="row" style="display: inline;padding-top: -10px;" >
-        <h1 style="text-align: center; color:#1b1b1b;margin-bottom: 3px;">Our past <strong>Events</strong></h1>
-        <div class="section-title-divider" style="width:300px;"></div>
-    </div>
-    <!--<div class="row" id="videoRow" style="display: inline; padding-top: -10px;">
-        <center>
-            <video id="video" style="margin-bottom: 30px" controls>
-                <source src="vid/event_video.MP4" type="video/mp4">
-                Your browser does not support the video tag.
-            </video>
-        </center>
-    </div>-->
-    <div class="row" style="display:inline;">
-        <div class="card-deck" style="width:80%;margin:auto;">
-            <div class="card" style="border-color: deepskyblue">
-                <img class="card-img-top" src="img/Events/event1.JPG" alt="Card image cap" style="height:400px;object-fit: contain;">
-                <div class="card-body" style="padding-left:0;padding-right:0;padding-bottom:0;">
-                    <h5 class="card-title" style="font-weight: bold;padding-left:10px;padding-right:10px;">Miss Dominican Republic 2019</h5>
-                    <p class="card-text" style="background-color: #D0FBFF; color=black;padding-left:10px;padding-right:10px;">The Coronation Ceremony of Miss Dominican Republic of Rhode Island 2019 & the presentation of Carnavalito by Emerson Brito
-                        <br><br>
-                        <strong>Date: June 23rd</strong><br>
-                        <strong>Tickets: $15</strong><br>
-                        <strong>Start: 4pm</strong><br>
-                    <small class=" d-flex justify-content-center">Formal Attire Preferred</small>
-                    </p>
-
-                </div>
-                <div id="content-desktop" class="card-footer" style="background-color: deepskyblue;height:50px;">
-                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
-                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
-                </div>
-
-                <div id="content-mobile" class="card-footer" style="background-color: deepskyblue;height:60px;">
-                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
-                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
-                </div>
-            </div>
-            <div class="card" style="border-color: deepskyblue">
-                <img class="card-img-top" src="img/Events/event2.JPG" alt="Card image cap" style="height:400px;object-fit: contain;">
-                <div class="card-body" style="padding-left:0;padding-right:0;padding-bottom:0;">
-                    <h5 class="card-title" style="font-weight: bold;padding-left:10px;padding-right:10px;">Miss Dominican Republic 2019</h5>
-                    <p class="card-text" style="background-color: #D0FBFF; color=black;padding-left:10px;padding-right:10px;">The Coronation Ceremony of Miss Dominican Republic of Rhode Island 2019 & the presentation of Carnavalito by Emerson Brito
-                        <br><br>
-                        <strong>Date: June 23rd</strong><br>
-                        <strong>Tickets: $15</strong><br>
-                        <strong>Start: 4pm</strong><br>
-                        <small class=" d-flex justify-content-center">Formal Attire Preferred</small>
-                    </p>
-
-                </div>
-                <div id="content-desktop" class="card-footer" style="background-color: deepskyblue;height:50px;">
-                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
-                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
-                </div>
-
-                <div id="content-mobile" class="card-footer" style="background-color: deepskyblue;height:60px;">
-                    <small class="d-flex justify-content-center" style="color:white;font-weight: bold; text-align: center">
-                        The Bomes Theater, 1017 Broad Street, Providence, RI 02905</small>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="col-md-6"></div>
 </div>
 
